a9164992be7709ac26aae6e740d47c716682e0ed
Emil Sedgh <emilsedgh@kde.org>
2023-09-15 00:39:17 -0700

    Proper

--- init.sql
+++ init.sql
@@ -1,8 +1,8 @@
 CREATE TABLE locks (
     id uuid not null primary key, 
-    created_at timestamp, 
-    unblocked_at timestamp, 
-    position int,
+    created_at timestamp without time zone, 
+    unblocked_at timestamp without time zone, 
+    position int NOT NULL DEFAULT 0,
 
     blocked_pid int, 
     blocked_user text, 
@@ -13,5 +13,5 @@ CREATE TABLE locks (
     blocking_user text, 
     blocking_statement text, 
     blocking_application text,
-    blocking_query_start timestamp
+    blocking_query_start timestamp without time zone
 );
